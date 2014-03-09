1f99a7d54f7e1810caae2a09756fdc4c94d3db29
Tapio Vierros <tapiovierros@gmail.com>
2014-03-09 21:12:08 +0200

    Window resize support.

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 function Game() {
 	this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-	this.camera.position.z = 8;
+	this.camera.position.z = 7;
 
 	this.actors = [];
 
@@ -11,6 +11,11 @@ function Game() {
 	this.renderer = new THREE.WebGLRenderer({ antialias: true });
 	this.renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(this.renderer.domElement);
+	window.addEventListener('resize', function() {
+		game.camera.aspect = window.innerWidth / window.innerHeight;
+		game.camera.updateProjectionMatrix();
+		game.renderer.setSize(window.innerWidth, window.innerHeight);
+	});
 
 	this.stats = new Stats();
 	this.stats.domElement.style.position = 'absolute';
