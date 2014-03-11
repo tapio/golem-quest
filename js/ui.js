1a36559856a1617265765abfa53219ad52d3dba0
Tapio Vierros <tapiovierros@gmail.com>
2014-03-11 22:02:46 +0200

    Improve in world msgs: fade animation and multiple at a time.

--- js/ui.js
+++ js/ui.js
@@ -3,21 +3,10 @@ function UI() {
 	this.scene = new THREE.Scene();
 
 	this.players = [];
+	this.elems = [];
 
 	this.renderer = new THREE.CSS3DRenderer();
 	document.getElementById("container").appendChild(this.renderer.domElement);
-
-	this.elems = [];
-	for (var i = 0; i < 3; ++i) {
-		var elem = document.createElement("div");
-		elem.className = "hud-elem";
-		var sprite = new THREE.CSS3DSprite(elem);
-		var scale = 0.02;
-		sprite.scale.set(scale, scale, scale);
-		document.body.appendChild(elem);
-		this.scene.add(sprite);
-		this.elems.push(sprite);
-	}
 
 	this.track = function(actor) {
 		this.players.push(actor);
@@ -25,14 +14,32 @@ function UI() {
 }
 
 UI.prototype.inWorldMsg = function(msg, position) {
-	var notif = this.elems[0];
-	notif.element.innerHTML = msg;
-	notif.position.x = position.x;
-	notif.position.y = position.y;
-	notif.position.z = 3;
+	var elem = document.createElement("div");
+	elem.className = "hud-elem";
+	elem.innerHTML = msg;
+	document.body.appendChild(elem);
+
+	var sprite = new THREE.CSS3DSprite(elem);
+	var scale = 0.02;
+	sprite.scale.set(scale, scale, scale);
+	sprite.position.set(position.x, position.y, 3);
+	sprite.timeout = Date.now() + 5000;
+
+	this.scene.add(sprite);
+	this.elems.push(sprite);
 };
 
 UI.prototype.render = function() {
 	this.renderer.render(this.scene, game.camera);
+
+	// Reap faded elements
+	var time = Date.now();
+	for (var i = this.elems.length - 1; i >= 0; --i) {
+		var elem = this.elems[i];
+		if (time >= elem.timeout) {
+			this.scene.remove(elem);
+			this.elems.splice(i, 1);
+		}
+	}
 };
 
