b6a697f90dae98f8ef9f4dab3d8732fcb72b724c
Tapio Vierros <tapiovierros@gmail.com>
2014-03-13 19:21:53 +0200

    Start crafting main menu.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
 "use strict";
 
-var cache = new Cache();
-var ui = new UI();
-var game = new Game();
-var clock = new THREE.Clock();
-
-var pl = new Actor({ model: randElem([ "golem-01", "golem-02", "golem-03"]), torch: true });
-pl.controller = new KeyboardController(0);
-ui.track(pl);
-
-//var pl2 = new Actor({ torch: true });
-//pl2.controller = new GamepadController(0);
-//game.addActor(pl2);
-//ui.track(pl2);
-
-for (var i = 0; i < 50; ++i) {
-	var monster = new Actor({ model: "ghoul", monster: true });
-	monster.position.x = (Math.random() * game.world.map.w)|0;
-	monster.position.y = (Math.random() * game.world.map.h)|0;
-}
-
-for (var i = 0; i < 50; ++i) {
-	game.spawnRandomItem();
-}
-
+var cache, ui, game, clock;
 
 function render() {
 	requestAnimationFrame(render);
@@ -35,4 +12,33 @@ function render() {
 	ui.render();
 };
 
-render();
+
+function start() {
+	dom("#intro").style.display = "none";
+	dom("#container").style.display = "block";
+	cache = new Cache();
+	ui = new UI();
+	game = new Game();
+	clock = new THREE.Clock();
+
+	var pl = new Actor({ model: randElem([ "golem-01", "golem-02", "golem-03"]), torch: true });
+	pl.controller = new KeyboardController(0);
+	ui.track(pl);
+
+	//var pl2 = new Actor({ torch: true });
+	//pl2.controller = new GamepadController(0);
+	//game.addActor(pl2);
+	//ui.track(pl2);
+
+	for (var i = 0; i < 50; ++i) {
+		var monster = new Actor({ model: "ghoul", monster: true });
+		monster.position.x = (Math.random() * game.world.map.w)|0;
+		monster.position.y = (Math.random() * game.world.map.h)|0;
+	}
+
+	for (var i = 0; i < 50; ++i) {
+		game.spawnRandomItem();
+	}
+
+	render();
+}
